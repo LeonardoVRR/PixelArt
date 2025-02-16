2227d3324b0667063b03e2489488bae425f5bcef
Leonardo Vitor <leonardovitor.rego@gmail.com>
2025-02-15 23:16:18 -0300

    Primeiro Commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const canvas = document.querySelector(".canvas")
+const paint_color = document.querySelector(".input-color")
+const screen_size = document.querySelector(".input-size")
+const save_img_btn = document.querySelector(".btn-save")
